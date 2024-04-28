d1c63a96297ce55637de69fae3e5f19fbbfe7c99
Jenina <jeninadelacruz521@yahoo.com>
2024-04-27 22:40:20 -0700

    fixed routers

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
 
 const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.use('/api', api);
 
-app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // GET Route for feedback page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 
